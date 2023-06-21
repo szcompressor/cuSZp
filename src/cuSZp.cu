bcd9d69f3a51534fa3bc94e9b0c5b4bed879e7ee
hyfshishen <478340079@qq.com>
2023-06-21 10:05:16 -0500

    updating and cleaning

--- src/cuSZp.cu
+++ src/cuSZp.cu
@@ -18,13 +18,13 @@ __device__ inline int get_bit_num(unsigned int x)
 
 __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, unsigned char* const __restrict__ cmpData, volatile unsigned int* const __restrict__ cmpOffset, volatile int* const __restrict__ flag, const float eb, const size_t nbEle)
 {
-    __shared__ unsigned int base_idx[cmp_tblock_size/32+1]; // optimizing for RTM in V100 GPU.
+    __shared__ unsigned int base_idx[cmp_tblock_size/32+1];
 
     const int tid = threadIdx.x;
     const int bid = blockIdx.x;
     const int idx = bid * blockDim.x + tid;
-    const int lane = tid & 31;  // lane in thread-block, optimizing for RTM in V100 GPU.
-    const int warp = tid >> 5;  // warp in thread-block, optimizing for RTM in V100 GPU.
+    const int lane = tid & 31;
+    const int warp = tid >> 5;
     const int block_num = cmp_chunk/32;
     const int warp_num = cmp_tblock_size/32;
     const int start_idx = idx * cmp_chunk;
@@ -44,7 +44,6 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
 
     for(int j=0; j<block_num; j++)
     {
-        // Block initilization.
         sign_flag[j] = 0;
         temp_start_idx = start_idx + j*32;
         temp_end_idx = temp_start_idx + 32;
@@ -52,33 +51,24 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
         prevQuant = 0;
         maxQuant = 0;
 
-        // Operation for each block
         for(int i=temp_start_idx; i<temp_end_idx; i++)
         {
-            // Get quantization and Lorenzo prediction
             quant_chunk_idx = i%cmp_chunk;
             currQuant = quantization(oriData[i], recipPrecision);
             lorenQuant = currQuant - prevQuant;
             prevQuant = currQuant;
-            // Get and combine sign info.
             sign_ofs = i % 32;
             sign_flag[j] |= (lorenQuant < 0) << (31 - sign_ofs);
-            // Get absolute quant.
             absQuant[quant_chunk_idx] = abs(lorenQuant);
-            // Update max quant.
             maxQuant = maxQuant > absQuant[quant_chunk_idx] ? maxQuant : absQuant[quant_chunk_idx];
         }
 
-        // Record block info.
         fixed_rate[j] = get_bit_num(maxQuant);
         thread_ofs += (fixed_rate[j]) ? (32+fixed_rate[j]*32) : 0;
-        // Write block fixed rate to compressed data.
         if(block_idx<rate_ofs) cmpData[block_idx] = (unsigned char)fixed_rate[j];
     }
     __syncthreads();
 
-
-    // Warp-level prefix-sum (inclusive).
     for(int i=1; i<32; i<<=1)
     {
         int tmp = __shfl_up_sync(0xffffffff, thread_ofs, i);
@@ -88,7 +78,6 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
     if(tid==0) base_idx[0] = 0;
     __syncthreads();
 
-    // Thread-block-level prefix-sum (exclusive).
     if(warp==0)
     {
         for(int i=1; i<32; i<<=1)
@@ -99,7 +88,6 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
     }
     __syncthreads();
 
-    // Write thread-block-level prefix-sum to global-memory.
     if(tid==cmp_tblock_size-1) 
     {
         cmpOffset[bid+1] = (base_idx[warp_num]+7)/8;
@@ -110,7 +98,6 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
     }
     __syncthreads();
 
-    // Global-level prefix-sum (exclusive).
     if(bid>0)
     {
         if(tid==cmp_tblock_size-1)
@@ -129,42 +116,34 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
     }
     __syncthreads();
 
-    // Assigning compression bytes by given prefix-sum results.
     if(!lane) base_idx[warp] = (base_idx[warp]+7)/8 + cmpOffset[bid] + rate_ofs;
     __syncthreads();
 
-    // Restore index.
     unsigned int prev_thread = __shfl_up_sync(0xffffffff, thread_ofs, 1);
     unsigned int cmp_byte_ofs;
     if(!lane) cmp_byte_ofs = base_idx[warp];
     else cmp_byte_ofs = base_idx[warp] + prev_thread / 8;
     
-    // Bit shuffle for each index.
     for(int j=0; j<block_num; j++)  
     {
         int chunk_idx_start = j*32;
 
-        // Operation for each block, if zero block then do nothing.
         if(fixed_rate[j])
         {
-            // Assign sign information for one block.
             cmpData[cmp_byte_ofs++] = 0xff & (sign_flag[j] >> 24);
             cmpData[cmp_byte_ofs++] = 0xff & (sign_flag[j] >> 16);
             cmpData[cmp_byte_ofs++] = 0xff & (sign_flag[j] >> 8);
             cmpData[cmp_byte_ofs++] = 0xff & sign_flag[j];
 
-            // Assign quant bit information for one block by bit-shuffle.
             unsigned char tmp_char0, tmp_char1, tmp_char2, tmp_char3;
             int mask = 1;
             for(int i=0; i<fixed_rate[j]; i++)
             {
-                // Initialization.
                 tmp_char0 = 0;
                 tmp_char1 = 0;
                 tmp_char2 = 0;
                 tmp_char3 = 0;
 
-                // Get ith bit in 0~7 quant, and store to tmp_char0.
                 tmp_char0 = (((absQuant[chunk_idx_start+0] & mask) >> i) << 7) |
                             (((absQuant[chunk_idx_start+1] & mask) >> i) << 6) |
                             (((absQuant[chunk_idx_start+2] & mask) >> i) << 5) |
@@ -174,7 +153,6 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
                             (((absQuant[chunk_idx_start+6] & mask) >> i) << 1) |
                             (((absQuant[chunk_idx_start+7] & mask) >> i) << 0);
 
-                // Get ith bit in 8~15 quant, and store to tmp_char1.
                 tmp_char1 = (((absQuant[chunk_idx_start+8] & mask) >> i) << 7) |
                             (((absQuant[chunk_idx_start+9] & mask) >> i) << 6) |
                             (((absQuant[chunk_idx_start+10] & mask) >> i) << 5) |
@@ -184,7 +162,6 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
                             (((absQuant[chunk_idx_start+14] & mask) >> i) << 1) |
                             (((absQuant[chunk_idx_start+15] & mask) >> i) << 0);
 
-                // Get ith bit in 16~23 quant, and store to tmp_char2.
                 tmp_char2 = (((absQuant[chunk_idx_start+16] & mask) >> i) << 7) |
                             (((absQuant[chunk_idx_start+17] & mask) >> i) << 6) |
                             (((absQuant[chunk_idx_start+18] & mask) >> i) << 5) |
@@ -194,7 +171,6 @@ __global__ void SZp_compress_kernel(const float* const __restrict__ oriData, uns
                             (((absQuant[chunk_idx_start+22] & mask) >> i) << 1) |
                             (((absQuant[chunk_idx_start+23] & mask) >> i) << 0);
                 
-                // Get ith bit in 24-31 quant, and store to tmp_char3.
                 tmp_char3 = (((absQuant[chunk_idx_start+24] & mask) >> i) << 7) |
                             (((absQuant[chunk_idx_start+25] & mask) >> i) << 6) |
                             (((absQuant[chunk_idx_start+26] & mask) >> i) << 5) |
@@ -238,7 +214,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
     int fixed_rate[block_num];
     unsigned int thread_ofs = 0;
 
-    // Obtain fixed rate information for each block.
     for(int j=0; j<block_num; j++)
     {
         block_idx = start_block_idx + j;
@@ -250,7 +225,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
     }
     __syncthreads();
 
-    // Warp-level prefix-sum (inclusive), also thread-block-level.
     for(int i=1; i<32; i<<=1)
     {
         int tmp = __shfl_up_sync(0xffffffff, thread_ofs, i);
@@ -258,7 +232,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
     }
     __syncthreads();
 
-    // Write warp(i.e. thread-block)-level prefix-sum to global-memory.
     if(lane==31) 
     {
         cmpOffset[warp+1] = (thread_ofs+7)/8;
@@ -269,7 +242,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
     }
     __syncthreads();
 
-    // Global-level prefix-sum (exclusive).
     if(warp>0)
     {
         if(!lane)
@@ -288,43 +260,35 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
     }
     __syncthreads();
 
-    // Retrieving compression bytes and reconstruct decompression data.
     if(!lane) base_idx = cmpOffset[warp] + rate_ofs;
     __syncthreads();
 
-    // Restore index.
     unsigned int prev_thread = __shfl_up_sync(0xffffffff, thread_ofs, 1);
     unsigned int cmp_byte_ofs;
     if(!lane) cmp_byte_ofs = base_idx;
     else cmp_byte_ofs = base_idx + prev_thread / 8;
 
-    // Restore bit-shuffle for each block.
     for(int j=0; j<block_num; j++)
     {
         temp_start_idx = start_idx + j*32;
         unsigned int sign_flag = 0;
 
-        // Operation for each block, if zero block then do nothing.
         if(fixed_rate[j])
         {
-            // Retrieve sign information for one block.
             sign_flag = (0xff000000 & (cmpData[cmp_byte_ofs++] << 24)) |
                         (0x00ff0000 & (cmpData[cmp_byte_ofs++] << 16)) |
                         (0x0000ff00 & (cmpData[cmp_byte_ofs++] << 8))  |
                         (0x000000ff & cmpData[cmp_byte_ofs++]);
             
-            // Retrieve quant data for one block.
             unsigned char tmp_char0, tmp_char1, tmp_char2, tmp_char3;
             for(int i=0; i<32; i++) absQuant[i] = 0;
             for(int i=0; i<fixed_rate[j]; i++)
             {
-                // Initialization.
                 tmp_char0 = cmpData[cmp_byte_ofs++];
                 tmp_char1 = cmpData[cmp_byte_ofs++];
                 tmp_char2 = cmpData[cmp_byte_ofs++];
                 tmp_char3 = cmpData[cmp_byte_ofs++];
 
-                // Get ith bit in 0~7 abs quant from global memory.
                 absQuant[0] |= ((tmp_char0 >> 7) & 0x00000001) << i;
                 absQuant[1] |= ((tmp_char0 >> 6) & 0x00000001) << i;
                 absQuant[2] |= ((tmp_char0 >> 5) & 0x00000001) << i;
@@ -334,7 +298,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
                 absQuant[6] |= ((tmp_char0 >> 1) & 0x00000001) << i;
                 absQuant[7] |= ((tmp_char0 >> 0) & 0x00000001) << i;
 
-                // Get ith bit in 8~15 abs quant from global memory.
                 absQuant[8] |= ((tmp_char1 >> 7) & 0x00000001) << i;
                 absQuant[9] |= ((tmp_char1 >> 6) & 0x00000001) << i;
                 absQuant[10] |= ((tmp_char1 >> 5) & 0x00000001) << i;
@@ -344,7 +307,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
                 absQuant[14] |= ((tmp_char1 >> 1) & 0x00000001) << i;
                 absQuant[15] |= ((tmp_char1 >> 0) & 0x00000001) << i;
 
-                // Get ith bit in 16-23 abs quant from global memory.
                 absQuant[16] |= ((tmp_char2 >> 7) & 0x00000001) << i;
                 absQuant[17] |= ((tmp_char2 >> 6) & 0x00000001) << i;
                 absQuant[18] |= ((tmp_char2 >> 5) & 0x00000001) << i;
@@ -354,7 +316,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
                 absQuant[22] |= ((tmp_char2 >> 1) & 0x00000001) << i;
                 absQuant[23] |= ((tmp_char2 >> 0) & 0x00000001) << i;
 
-                // // Get ith bit in 24-31 abs quant from global memory.
                 absQuant[24] |= ((tmp_char3 >> 7) & 0x00000001) << i;
                 absQuant[25] |= ((tmp_char3 >> 6) & 0x00000001) << i;
                 absQuant[26] |= ((tmp_char3 >> 5) & 0x00000001) << i;
@@ -364,8 +325,6 @@ __global__ void SZp_decompress_kernel(float* const __restrict__ decData, const u
                 absQuant[30] |= ((tmp_char3 >> 1) & 0x00000001) << i;
                 absQuant[31] |= ((tmp_char3 >> 0) & 0x00000001) << i;
             }
-            
-            // Delorenzo and store data back to decompression data.
             prevQuant = 0;
             for(int i=0; i<32; i++)
             {
