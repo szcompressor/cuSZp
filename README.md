f89f559617a984d6f554ea0774583ebb227e32fa
hyfshishen <478340079@qq.com>
2023-06-21 10:01:12 -0500

    updating

---
+++ README.md
@@ -0,0 +1,19 @@
+# cuSZp
+cuSZp is a lossy error-bounded compression library for compression of floating-point data in NVIDIA GPU.
+
+## Environment Requirements
+### Common
+- Git 2.15 or newer
+- CMake 3.21 or newer
+- Cuda Toolkit >= 11.0
+
+## Installation
+To build cuSZp type:
+```
+cd cuSZp
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=path/to/your/cuszp/installation/directory ..
+make -j
+make install
+```
---
+++ include/cuSZp_entry.h
@@ -0,0 +1,11 @@
+#ifndef CUSZP_INCLUDE_CUSZP_ENTRY_H
+#define CUSZP_INCLUDE_CUSZP_ENTRY_H
+
+#include <cuda_runtime.h>
+
+void SZp_compress_hostptr(float* oriData, unsigned char* cmpBytes, size_t nbEle, size_t* cmpSize, float errorBound);
+void SZp_decompress_hostptr(float* decData, unsigned char* cmpBytes, size_t nbEle, size_t cmpSize, float errorBound);
+void SZp_compress_deviceptr(float* d_oriData, unsigned char* d_cmpBytes, size_t nbEle, size_t* cmpSize, float errorBound, cudaStream_t stream = 0);
+void SZp_decompress_deviceptr(float* d_decData, unsigned char* d_cmpBytes, size_t nbEle, size_t cmpSize, float errorBound, cudaStream_t stream = 0);
+
+#endif // CUSZP_INCLUDE_CUSZP_ENTRY_H
